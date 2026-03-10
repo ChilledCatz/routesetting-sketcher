7ee0822ff65f5bb978ef4bb721a38ae3239ad951
Kat Sanders <kat@plainwater.nl>
2026-03-10 15:46:45 +0100

    first test scene with adjustable controls

---
+++ src/lib/test/App.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import { Pane, Slider } from 'svelte-tweakpane-ui'
+    import { Canvas } from '@threlte/core';
+    import Scene from './Scene.svelte';
+	import { Stars } from '@threlte/extras';
+
+    let x = $state(5);
+    let y = $state(5);
+    let tilt = $state(0);
+</script>
+
+<Pane
+    title="size"
+    position="fixed"
+>
+    <Slider
+        bind:value={x}
+        min={1}
+        max={10}
+        format={(v) => v.toFixed(2)}
+        label='width'
+    />
+    <Slider
+        bind:value={y}
+        min={1}
+        max={10}
+        format={(v) => v.toFixed(2)}
+        label='height'
+    />
+    <Slider
+        bind:value={tilt}
+        min={-1}
+        max={1}
+        format={(v) => v.toFixed(2)}
+        label='tilt'
+    />
+</Pane>
+
+<div class="bg-slate-800">
+    <Canvas>
+        <Stars />
+        <Scene x={x} y={y} tilt={tilt} />
+    </Canvas>
+</div>
+
+<style>
+    div {
+        height: 100%;
+    }
+</style>
---
+++ src/lib/test/Scene.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { T } from '@threlte/core';
+	import { OrbitControls } from '@threlte/extras';
+
+    let { x, y, tilt } = $props();
+</script>
+
+<T.PerspectiveCamera 
+    makeDefault
+    position={[10, 10, 10]}
+    oncreate={(ref) => {
+        ref.lookAt(0, 1, 0);
+    }}
+/>
+
+<T.Mesh rotation={[tilt, 0, 0]}>
+	<T.PlaneGeometry args={[x, y]} />
+	<T.MeshBasicMaterial color="blue" />
+</T.Mesh>
